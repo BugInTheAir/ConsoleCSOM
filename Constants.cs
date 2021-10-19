4c0792337ea682f94f6d6fb712f90020f846aa86
Bug In The Air <58814071+BugInTheAir@users.noreply.github.com>
2021-10-19 16:02:19 +0700

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -8,6 +8,12 @@ namespace ConsoleCSOM
 {
     public class Constants
     {
-
+        public static string CSOM_TEST = "CSOM Test";
+        public static string CSOM_TEST_CONTENT_TYPE = "CSOM Test content type";
+        public static string AUTHOR_FIELD_NAME = "authorFN";
+        public static string MY_VIEW_TITLE = "Test View";
+        public static string ABOUT = "aboutCT";
+        public static string CITY = "cityInfo";
+        public static string TITLE = "Title";
     }
 }
