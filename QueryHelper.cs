65cef1291a804c9f2986c567450d35f28bca1a6b
Bug In The Air <58814071+BugInTheAir@users.noreply.github.com>
2021-10-20 18:33:58 +0700

    Create QueryHelper.cs

---
+++ QueryHelper.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCSOM
+{
+    public class QueryHelper
+    {
+        public static CamlQuery GetQueryByTitle(string titleName, string relativePath = null)
+        {
+            if(relativePath is null)
+            {
+                return new CamlQuery()
+                {
+                    ViewXml = @$"<View>
+                                <Query>
+                                    <Where>
+                                         <Eq>
+                                            <FieldRef Name=""Title""></FieldRef>
+                                            <Value Type=""Text"">{titleName}</Value>
+                                          </Eq>
+                                  </Where>
+                                </Query>
+                            </View>"
+                };
+            }
+            return new CamlQuery()
+            {
+                ViewXml = @$"<View>
+                                <Query>
+                                    <Where>
+                                         <Eq>
+                                            <FieldRef Name=""Title""></FieldRef>
+                                            <Value Type=""Text"">{titleName}</Value>
+                                          </Eq>
+                                  </Where>
+                                </Query>
+                            </View>",
+                FolderServerRelativeUrl = relativePath
+            };
+        }
+    }
+}
